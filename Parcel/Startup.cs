9578ea5c61633cfcb1fd614fe1ac1c9c003251ff
Nikhil Batta and Rachael Bouissey <nikhil@>
2019-10-08 14:13:16 -0700

    add linking to css file

--- Parcel/Startup.cs
+++ Parcel/Startup.cs
@@ -33,6 +33,8 @@ namespace ToDoList
           template: "{controller=Home}/{action=Index}/{id?}");
       });
 
+       app.UseStaticFiles(); 
+
         app.Run(async (context) =>
         {
           await context.Response.WriteAsync("Something went wrong!");
